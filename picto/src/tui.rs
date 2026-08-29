ba4b68804f0dc9bebf702cd8e041f00c49a4feb3
Magi <paoloimbrmagi@gmail.com>
2026-08-29 12:15:16 +0200

    another progress

--- picto/src/tui.rs
+++ picto/src/tui.rs
@@ -17,3 +17,52 @@ pub struct Tui {
     pub terminal: CrosstermTerminal,
     pub events: EventHandler,
 }
+
+impl Tui {
+    pub fn new(terminal: CrosstermTerminal, events: EventHandler) -> Self {
+        Self { terminal, events }
+    }
+
+    /// Enter the terminal interface
+    ///
+    /// Enters raw mode and sets terminal properties
+    pub fn enter(&mut self) -> Result<()> {
+        terminal::enable_raw_mode()?;
+
+        // Definition custom panic to reset terminal properties
+        let panic_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic| {
+            Self::reset().expect("Unable to reset the terminal");
+            panic_hook(panic);
+        }));
+        todo!()
+    }
+
+    /// Resets the terminal interface.
+    ///
+    /// This function is also used for the panic hook to revert
+    /// the terminal properties if unexpected errors occur.
+    fn reset() -> Result<()> {
+        terminal::disable_raw_mode()?;
+        execute!(io::stderr(), LeaveAlternateScreen, DisableMouseCapture)?;
+        Ok(())
+    }
+
+    /// Exits the terminal interface.
+    ///
+    /// It disables the raw mode and reverts back the terminal properties.
+    pub fn exit(&mut self) -> Result<()> {
+        Self::reset()?;
+        self.terminal.show_cursor()?;
+        Ok(())
+    }
+
+    /// [`Draw`] the terminal interface by [`rendering`] the widgets.
+    ///
+    /// [`Draw`]: tui::Terminal::draw
+    /// [`rendering`]: crate::ui:render
+    pub fn draw(&mut self) -> Result<()> {
+        self.terminal.draw(|frame| ui::render(app, frame))?;
+        Ok(())
+    }
+}
